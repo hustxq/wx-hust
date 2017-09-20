d35fc809896653fb4c140c875a0a65f7b3625a75
hustxq <26315874@qq.com>
2017-09-20 16:36:31 +0800

    init
    Signed-off-by: hustxq <26315874@qq.com>

---
+++ src/main/java/cn/hustxq/hust/HustApplication.java
@@ -0,0 +1,12 @@
+package cn.hustxq.hust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustApplication.class, args);
+	}
+}
